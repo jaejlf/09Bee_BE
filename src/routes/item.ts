e09db951c91d49c100c0d794c419d41df82d84de
ympark99 <ympark1230@naver.com>
2021-11-21 01:14:47 +0900

    추가 API 리스트 작성

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -3,6 +3,9 @@ import itemController from "../controllers/item";
 
 const router = express.Router()
 
-router.get("/:itemId", itemController.getItem);
+router.get("/:itemId", itemController.getItem); // 개별 아이템 API
+router.get("/dobbyIn/:userId", itemController.getItem); // 더비 공동구매 참여
+router.get("/progress/:progressId", itemController.getItem); // 러비 마감 상태 변경 등 공구 진행 상태 변경API
+router.get("/notice", itemController.getItem); // 러비 공지사항 작성
 
 export = router;
