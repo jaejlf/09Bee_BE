123990719558e4530cdbf0d103c93959fb2d29c6
ympark99 <ympark1230@naver.com>
2021-11-21 15:51:02 +0900

    공지사항 API 알고리즘 개발

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -6,6 +6,6 @@ const router = express.Router()
 router.get("/:itemId", itemController.getItem); // 개별 아이템 API
 router.get("/dobbyIn/:userId", itemController.dobbyIn); // 더비 공동구매 참여
 router.get("/progress/:progressId", itemController.changeProgress); // 러비 마감 상태 변경 등 공구 진행 상태 변경API
-router.get("/notice", itemController.getItem); // 러비 공지사항 작성
+router.post("/notice", itemController.makeNotice); // 러비 공지사항 작성
 
 export = router;
