2abcb3f0aa63c429077de48db6d5a75b9b1a24cf
jaejeong <78673570+jaejlf@users.noreply.github.com>
2021-11-22 03:10:43 +0900

    참여 더비 리스트 API

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -20,14 +20,14 @@ const getItem = async (req: Request, res: Response, next: NextFunction) => {
             "targetNum": item?.targetNum,
             "progress": item?.progress,
             "notice": item?.notice,
-            "price" : item?.price,
-            "date" : item?.date
+            "price": item?.price,
+            "date": item?.date
         }
         res.status(200).json(
             exportItem
-        )        
+        )
     }
-    catch (error : any) {
+    catch (error: any) {
         res.status(500).json({
             error: error.message
         })
@@ -40,7 +40,7 @@ const itemFindOne = async (id: number) => {
         return await Item.findOne(
             { itemId: id })
     }
-    catch (error : any) {
+    catch (error: any) {
         console.log("itemFindOne" + error.message)
     }
 }
@@ -53,7 +53,7 @@ const itemFindUpdateSet = async (id: number, param: any) => {
             { $set: param },
             { new: true })
     }
-    catch (error : any) {
+    catch (error: any) {
         console.log("itemFindUpdate" + error.message)
     }
 }
@@ -66,7 +66,7 @@ const itemFindUpdateInc = async (id: number, param: any) => {
             { $inc: param },
             { new: true })
     }
-    catch (error : any) {
+    catch (error: any) {
         console.log("itemFindUpdate" + error.message)
     }
 }
@@ -81,30 +81,50 @@ const dobbyIn = async (req: Request, res: Response, next: NextFunction) => {
             res.status(501).json({
                 error: "해당 itemId에 맞는 item이 없습니다."
             })
-        } else if(foundItemInfo.targetNum.currentNum < foundItemInfo.targetNum.maxNum) { // 현재 인원 < 최대인원일시 배열에 추가하여 update
+        } else if (foundItemInfo.targetNum.currentNum < foundItemInfo.targetNum.maxNum) { // 현재 인원 < 최대인원일시 배열에 추가하여 update
             const dobbyIDs: Array<number> = foundItemInfo.dobbyIDs; // 업데이트할 배열 선언
             // dobbyIDs 배열 뒤에 userId붙이기
             dobbyIDs.push(userId);
+
             // db에 업데이트
-            await itemFindUpdateSet( itemId, { dobbyIDs : dobbyIDs });
+            await itemFindUpdateSet(itemId, { dobbyIDs: dobbyIDs });
 
             // currentNum ++시키기
-            await itemFindUpdateInc( itemId, { "targetNum.currentNum" : 1 });          
+            await itemFindUpdateInc(itemId, { "targetNum.currentNum": 1 });
 
             // 모집 인원 달성 시 && 알람 비어 있을 시 충족 멘트 lobbyAlarm에 추가
-            if(foundItemInfo.targetNum.minNum <= foundItemInfo.targetNum.currentNum && foundItemInfo.lobbyAlarm.length === 0){
+            if (foundItemInfo.targetNum.minNum <= foundItemInfo.targetNum.currentNum && foundItemInfo.lobbyAlarm.length === 0) {
                 const lobbyAlarms: Array<string> = foundItemInfo.lobbyAlarm; // 업데이트할 배열 선언
-                const addAlarm : string = "진행 중인 '" + foundItemInfo.title + "'의 공구모집 최소 인원이 충족되었습니다. 주문을 진행해보세요!";
+                const addAlarm: string = "진행 중인 '" + foundItemInfo.title + "'의 공구모집 최소 인원이 충족되었습니다. 주문을 진행해보세요!";
                 lobbyAlarms.push(addAlarm);
-                await itemFindUpdateSet( itemId, { lobbyAlarm : lobbyAlarms });                
+                await itemFindUpdateSet(itemId, { lobbyAlarm: lobbyAlarms });
             }
 
             res.status(200).json({
                 result: true
             })
-        }    
+        }
     }
-    catch (error : any) {
+    catch (error: any) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+}
+
+// 참여 더비 리스트 확인
+const getDobbyList = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const itemId = parseInt(req.params.itemId);
+        const item = await Item.findOne({ itemId: itemId }); 
+        const exportList = {
+            "dobbyIDs": item?.dobbyIDs
+        }
+        res.status(200).json(
+            exportList
+        )
+    }
+    catch (error: any) {
         res.status(500).json({
             error: error.message
         })
@@ -121,24 +141,24 @@ const changeProgress = async (req: Request, res: Response, next: NextFunction) =
             res.status(501).json({
                 error: "해당 itemId에 맞는 item이 없습니다."
             })
-        } else{ 
+        } else {
             // db에 업데이트
-            await itemFindUpdateSet( itemId, { progress : progressId });          
-            
+            await itemFindUpdateSet(itemId, { progress: progressId });
+
             // 공구 모집 마감 시 더비에게 알람
-            if(progressId === 3){
+            if (progressId === 3) {
                 const dobbyAlarms: Array<string> = foundItemInfo.dobbyAlarm; // 업데이트할 배열 선언
-                const addAlarm : string = "참여 중인 '" + foundItemInfo.title + "'의 공구모집 공구 모집이 종료되었습니다. 확인해보세요";
+                const addAlarm: string = "참여 중인 '" + foundItemInfo.title + "'의 공구모집 공구 모집이 종료되었습니다. 확인해보세요";
                 dobbyAlarms.push(addAlarm);
-                await itemFindUpdateSet( itemId, { dobbyAlarm : dobbyAlarms });  
+                await itemFindUpdateSet(itemId, { dobbyAlarm: dobbyAlarms });
             }
 
             res.status(200).json({
                 progress: progressId
             })
-        }    
+        }
     }
-    catch (error : any) {
+    catch (error: any) {
         res.status(500).json({
             error: error.message
         })
@@ -146,7 +166,7 @@ const changeProgress = async (req: Request, res: Response, next: NextFunction) =
 }
 
 // 공지사항 작성
-const makeNotice = async (req : Request, res : Response, next : NextFunction) => {
+const makeNotice = async (req: Request, res: Response, next: NextFunction) => {
     try {
         let { itemId, notice } = req.body;
         const foundItemInfo: any = await itemFindOne(itemId); // 아이템 api 데이터 가져옴
@@ -155,24 +175,24 @@ const makeNotice = async (req : Request, res : Response, next : NextFunction) =>
                 message: "해당 itemId에 맞는 item이 없습니다."
             })
         } else {
-                const notices: Array<string> = foundItemInfo.notice; // 업데이트할 배열 선언
-                // notice 배열 뒤에 새 공지사항 붙이기
-                notices.push(notice);
-                // db에 업데이트
-                await itemFindUpdateSet( itemId, { notice: notices });
+            const notices: Array<string> = foundItemInfo.notice; // 업데이트할 배열 선언
+            // notice 배열 뒤에 새 공지사항 붙이기
+            notices.push(notice);
+            // db에 업데이트
+            await itemFindUpdateSet(itemId, { notice: notices });
 
-                // 공지사항 작성 시 더비에게 알람
-                const dobbyAlarms: Array<string> = foundItemInfo.dobbyAlarm; // 업데이트할 배열 선언
-                const addAlarm : string = "참여 중인 '" + foundItemInfo.title + "'의 새로운 공지사항이 업로드되었습니다. 공지사항을 확인해보세요!";
-                dobbyAlarms.push(addAlarm);
-                await itemFindUpdateSet( itemId, { dobbyAlarm : dobbyAlarms });                              
+            // 공지사항 작성 시 더비에게 알람
+            const dobbyAlarms: Array<string> = foundItemInfo.dobbyAlarm; // 업데이트할 배열 선언
+            const addAlarm: string = "참여 중인 '" + foundItemInfo.title + "'의 새로운 공지사항이 업로드되었습니다. 공지사항을 확인해보세요!";
+            dobbyAlarms.push(addAlarm);
+            await itemFindUpdateSet(itemId, { dobbyAlarm: dobbyAlarms });
 
-                res.status(200).json({
-                    result: "공지사항 등록 완료"
-                })
-            }
-        }    
-    catch (error : any) {
+            res.status(200).json({
+                result: "공지사항 등록 완료"
+            })
+        }
+    }
+    catch (error: any) {
         res.status(500).json({
             message: error.message
         })
@@ -183,5 +203,6 @@ export default {
     getItem,
     dobbyIn,
     changeProgress,
-    makeNotice
+    makeNotice,
+    getDobbyList
 };
--- src/routes/item.ts
+++ src/routes/item.ts
@@ -7,5 +7,6 @@ router.get("/:itemId", itemController.getItem); // 개별 아이템 API
 router.get("/dobbyIn/:userId", itemController.dobbyIn); // 더비 공동구매 참여
 router.get("/progress/:progressId", itemController.changeProgress); // 러비 마감 상태 변경 등 공구 진행 상태 변경API
 router.post("/notice", itemController.makeNotice); // 러비 공지사항 작성
+router.get("/getDobbyList/:itemId", itemController.getDobbyList); //참여 더비 리스트 확인
 
 export = router;
